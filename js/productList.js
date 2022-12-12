a899160f38afe1b7727bd6b024723871d334095e
Vy Do Huynh Thao <75234626+thaovy2709@users.noreply.github.com>
2022-12-12 23:31:46 +0700

    product list + product detail + checkout + admin dashboard

--- js/productList.js
+++ js/productList.js
@@ -1,4 +1,4 @@
-fetch("./productList.json")
+fetch("/data/productList.json")
 .then(function(response){
     return response.json();
 })
@@ -7,28 +7,38 @@ fetch("./productList.json")
     let out ="";
     for(let item of productList){
         out +=  `
-        <div style="width:225px;height:225px;overflow:hidden">
-          <img class="item-img" src='${item.image}'>
-        </div>
-        <div class="item-content">
-          <div class="item-name">
-            <h3>${item.name}</h3>
-            <img src='${item.wishlist}'>
+        <a href="productdetail.html">
+        <div class="product-detail">
+          <div style="width:225px;height:225px;overflow:hidden">
+            <img class="item-img" src="/img/ProductList/daisy1.png">
           </div>
-          <div class="rating">
-            <img src='${item.rating-star}'>
-            <div class="rate">
-              <p>${item.rating}</p>
-              <span>${item.stock}</span>
+          <div class="item-content">
+            <div class="item-name">
+              <h3>Daisy</h3>
+              <img src="/img/icon/addtowishlist.svg">
+            </div>
+            <div class="rating">
+              <img src="/img/icon/yellowstar.svg">
+              <div class="rate">
+                <p>4.9</p>
+                <span>Còn 50 sản phẩm</span>
+              </div>
+            </div>
+            <div class="unit">
+              <p>Đơn vị: cái</p>
+            </div>
+            <div class="price">
+              <h3 class="sale-price">250.000 vnđ</h3>
             </div>
           </div>
-          <div class="unit">
-            <p>${item.unit}</p>
-          </div>
-          <div class="price">
-            <h3 class="sale-price">${item.price}</h3>
-          </div>
         </div>
+        <button class="btnDesign">
+          <div class="btnOrderText">
+            <img src="/img/icon/BagRed.svg">
+            <h3>Đặt mua</h3>
+          </div>
+        </button>
+        </a>
         `;
     }
     placeholder.innerHTML = out;
