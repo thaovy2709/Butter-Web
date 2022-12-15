af3085a324643dd91eff20092e183f0d1459e98f
nguyenphamthuyanh <116698530+nguyenphamthuyanh@users.noreply.github.com>
2022-12-15 13:16:18 +0700

    updated

---
+++ js/myaccount.js
@@ -0,0 +1,11 @@
+function cannotChange() {
+    alert ("Dữ liệu này không thể thay đổi");
+}
+
+var data=[]
+function updateData() {
+    console.log('Button is clicked');
+    alert("Cập nhật dữ liệu thành công")
+
+    }
+
